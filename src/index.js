c6f162eef1ccaef2ecbeaf7bcc088c3a10780bed
Thịnh Lê <20211tt0718@mail.tdc.edu.vn>
2022-11-03 16:37:33 +0700

    Install

--- src/index.js
+++ src/index.js
@@ -33,6 +33,6 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 //Route init
 route(app);
 
-app.listen(port, () => {
+                    app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
