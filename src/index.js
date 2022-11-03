acc1954159009f102bafada12fd041055cfa0b96
Thịnh Lê <20211tt0718@mail.tdc.edu.vn>
2022-11-03 14:29:15 +0700

    Refacter code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //Template Engine
 app.engine('hbs', exphbs.engine({
@@ -17,15 +25,12 @@ app.engine('hbs', exphbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-//Route 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//Route init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
